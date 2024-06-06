6492432696ef0facda912a7351ca62ac7ad7f914
Sandro Domianidze <sdomianidze123@gmail.com>
2024-06-06 19:20:42 +0400

    feat(tasks.index): setup a route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
 });
+
+Route::get('/tasks', function () {
+    return 'tasks.index';
+})->name('tasks.index');
