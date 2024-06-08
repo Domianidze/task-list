9eec63277fa2bbb619852d0d5e787b85290fbb10
Sandro Domianidze <sdomianidze123@gmail.com>
2024-06-08 22:30:00 +0400

    refactor: utilize model create and update methods

--- routes/web.php
+++ routes/web.php
@@ -23,24 +23,13 @@ Route::get('/tasks/{task}/edit', function (Task $task) {
 })->name('tasks.edit');
 
 Route::post('/tasks', function (TaskRequest $request) {
-    $data = $request->validated();
-
-    $task = new Task();
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+    $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task has been added successfully!');
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
-    $data = $request->validated();
-
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+    $task->update($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task has been edited successfully!');
 })->name('tasks.update');
