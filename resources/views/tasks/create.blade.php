0ed379208237b8e527a492a246573714d827f05f
Sandro Domianidze <sdomianidze123@gmail.com>
2024-06-08 20:26:11 +0400

    feat(forms): persist values on validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -16,21 +16,21 @@
   @csrf
   <label for="title">
     <div>Title:</div>
-    <input type="text" name="title" id="title" />
+    <input type="text" name="title" id="title" value="{{old('title')}}" />
     @error('title')
     <div class="error-message">{{$message}}</div>
     @enderror
   </label>
   <label for="description">
     <div>Description:</div>
-    <textarea type="text" name="description" id="description" rows="3"></textarea>
+    <textarea type="text" name="description" id="description" rows="3">{{old('description')}}</textarea>
     @error('description')
     <div class="error-message">{{$message}}</div>
     @enderror
   </label>
   <label for="long_description">
     <div>Long description:</div>
-    <textarea type="text" name="long_description" id="long_description" rows="6"></textarea>
+    <textarea type="text" name="long_description" id="long_description" rows="6">{{old('long_description')}}</textarea>
     @error('long_description')
     <div class="error-message">{{$message}}</div>
     @enderror
