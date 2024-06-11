4c309ce5bb3e0ff02dd95167e812abbcdc055e7c
Sandro Domianidze <sdomianidze123@gmail.com>
2024-06-11 19:08:35 +0400

    feat: make flash messages closable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,14 +37,14 @@
   </style>
 </head>
 
-<body class="container mx-auto mt-10 mb-10 max-w-lg">
+<body x-data="{open: true}" class="container mx-auto mt-10 mb-10 max-w-lg">
   @if(session()->has('success'))
-  <div class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700" role="alert">
+  <div x-show="open" class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700" role="alert">
     <strong class="font-bold">Success!</strong>
     <div>{{ session('success') }}</div>
 
-    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 cursor-pointer">
+    <span @click="open = false" class="absolute top-0 bottom-0 right-0 px-4 py-3 cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
       </svg>
     </span>
